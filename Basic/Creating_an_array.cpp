765391a3e15789b01861a67aabc85641ddab05b4
Paari-Mannan-M <paarimannanm@gmail.com>
2026-08-27 14:16:50 +0530

    Enhance array creation with user input
    Modified the program to allow user input for array size and elements, and corrected syntax for input/output.

--- Basic/Creating_an_array.cpp
+++ Basic/Creating_an_array.cpp
@@ -1,11 +1,18 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main()
-{
- int arr[n];
-
-for(int i=0;i<n;i++)
-  cout<<arr[i]<<" ";
-
-return 0;
+int main() {
+    int n;
+    cout << "Enter size of array: ";
+    cin >> n;
+    int arr[n];
+    cout << "Enter " << n << " elements: ";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+    cout << "Array elements are: ";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    return 0;
 }
+
